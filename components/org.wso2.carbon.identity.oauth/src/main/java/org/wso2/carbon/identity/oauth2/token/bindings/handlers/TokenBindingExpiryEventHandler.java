1fdc5e6b44fa60888a4a9737f5edb94f683e0ee1
shanggeeth <shanggeethk@gmail.com>
2024-06-12 18:12:25 +0530

    Use token Binding type from logout request

--- components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/bindings/handlers/TokenBindingExpiryEventHandler.java
+++ components/org.wso2.carbon.identity.oauth/src/main/java/org/wso2/carbon/identity/oauth2/token/bindings/handlers/TokenBindingExpiryEventHandler.java
@@ -205,13 +205,13 @@ public class TokenBindingExpiryEventHandler extends AbstractEventHandler {
                                             String bindingType) throws IdentityOAuth2Exception,
             InvalidOAuthClientException, OAuthSystemException {
 
-        revokeTokensOfBindingRef(user, getBindingRefFromType(request, consumerKey, bindingType));
+        revokeTokensOfBindingRef(user, getBindingRefFromType(request, consumerKey, bindingType), bindingType);
     }
 
     private void revokeTokensForCommonAuthCookie(HttpServletRequest request, AuthenticatedUser user) throws
             IdentityOAuth2Exception, InvalidOAuthClientException {
 
-        revokeTokensOfBindingRef(user, getBindingRefFromCommonAuthCookie(request));
+        revokeTokensOfBindingRef(user, getBindingRefFromCommonAuthCookie(request), null);
     }
 
     /**
@@ -289,11 +289,12 @@ public class TokenBindingExpiryEventHandler extends AbstractEventHandler {
      *
      * @param user                  authenticated user
      * @param tokenBindingReference token binding reference
+     * @param tokenBindingType      token binding type for the current binding reference
      * @throws IdentityOAuth2Exception     if an exception occurs while revoking tokens
      * @throws InvalidOAuthClientException if an exception occurs while revoking tokens
      */
-    private void revokeTokensOfBindingRef(AuthenticatedUser user, String tokenBindingReference) throws
-            IdentityOAuth2Exception, InvalidOAuthClientException {
+    private void revokeTokensOfBindingRef(AuthenticatedUser user, String tokenBindingReference, String tokenBindingType)
+            throws IdentityOAuth2Exception, InvalidOAuthClientException {
 
         if (StringUtils.isBlank(tokenBindingReference) || user == null) {
             return;
@@ -333,7 +334,7 @@ public class TokenBindingExpiryEventHandler extends AbstractEventHandler {
                         revokeFederatedTokens(consumerKey, user, accessTokenDO, tokenBindingReference);
                     } else if (StringUtils.equalsIgnoreCase(userId, authenticatedUser.getUserId()) ||
                             (OAuth2Util.isRevokeAllTokensOfTokenOfSSOSession() &&
-                                    StringUtils.equalsIgnoreCase(accessTokenDO.getTokenBinding().getBindingType(),
+                                    StringUtils.equalsIgnoreCase(tokenBindingType,
                                             OAuth2Constants.TokenBinderType.SSO_SESSION_BASED_TOKEN_BINDER))) {
                         revokeTokens(consumerKey, accessTokenDO, tokenBindingReference);
                     }

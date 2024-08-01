d886ddd915828ec8393ca2ea097967ba28c54edd
Yasasr1 <yasasramanayaka@gmail.com>
2024-08-01 12:17:34 +0530

    Improvements to client attestation validation.

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -4637,8 +4637,7 @@ public class OAuth2AuthzEndpoint {
         if (clientAttestationContextObj instanceof ClientAttestationContext) {
             clientAttestationContext = (ClientAttestationContext) clientAttestationContextObj;
         } else {
-            clientAttestationContext = new ClientAttestationContext();
-            clientAttestationContext.setAttestationEnabled(false);
+            clientAttestationContext = new ClientAttestationContext(false);
             clientAttestationContext.setAttested(false);
         }
         return clientAttestationContext;

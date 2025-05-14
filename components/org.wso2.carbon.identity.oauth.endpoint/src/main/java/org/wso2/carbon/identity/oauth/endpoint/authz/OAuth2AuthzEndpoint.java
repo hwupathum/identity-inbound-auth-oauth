4a51199dda045a426465c6a1b6e4586e3ffeece5
Shenali <shenali@wso2.com>
2025-05-14 12:10:16 +0530

    Fix checkstyle issues

--- components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
+++ components/org.wso2.carbon.identity.oauth.endpoint/src/main/java/org/wso2/carbon/identity/oauth/endpoint/authz/OAuth2AuthzEndpoint.java
@@ -40,7 +40,6 @@ import org.apache.oltu.oauth2.common.error.OAuthError;
 import org.apache.oltu.oauth2.common.exception.OAuthProblemException;
 import org.apache.oltu.oauth2.common.exception.OAuthSystemException;
 import org.apache.oltu.oauth2.common.message.OAuthResponse;
-import org.apache.xmlbeans.impl.common.IdentityConstraint;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.owasp.encoder.Encode;
